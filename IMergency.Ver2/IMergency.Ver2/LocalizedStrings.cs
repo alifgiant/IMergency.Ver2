35248ddf34caee1a80c9ed61611300df4e38ee7c
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2014-03-13 14:20:34 +0700

    Black Project

---
+++ IMergency.Ver2/IMergency.Ver2/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using IMergency.Ver2.Resources;
+
+namespace IMergency.Ver2
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
